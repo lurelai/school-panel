2dea94a998c318633fe88f11b332d1927db5f662
Lucas Ariel <github.lucas.ariel27@gmail.com>
2024-02-27 01:33:04 -0300

    init to serve css

--- app.js
+++ app.js
@@ -3,6 +3,8 @@ const path = require('path')
 
 const app = express()
 
+app.use('/css', express.static(path.join(__dirname, '/public/css')))
+
 app.get('/', (req, res)=>{
     res.sendFile(path.join(__dirname, 'views/index.html'))
 })
