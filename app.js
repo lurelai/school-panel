d18723d6261c23ff0944f022b8eaf4bf63d7b61d
Lucas Ariel <github.lucas.ariel27@gmail.com>
2024-03-09 00:56:33 -0300

    define good habits

--- app.js
+++ app.js
@@ -3,19 +3,27 @@ const path = require('path')
 
 const app = express()
 
+
 app.use(express.json())
 app.use(express.urlencoded({ extended: true }))
+
 
 app.use('/css', express.static(path.join(__dirname, '/public/css')))
 app.use('/js', express.static(path.join(__dirname, '/public/js')))
 
+// Db connection
+require('./src/database/db').createConnection()
+
+
 // Index route
 app.get('/', (req, res)=>{ res.sendFile(path.join(__dirname, 'views/index.html')) })
+
 
 // Routes
 require('./src/routes/studentRoute')(app)
 require('./src/routes/teacherRoute')(app)
 require('./src/routes/adminRoute')(app)
 
-app.listen(8080, ()=>{console.log(" Connected ")})
+
+app.listen(8080, ()=>{console.log("Connected")})
 
--- src/database/db.js
+++ src/database/db.js
@@ -3,19 +3,31 @@ const { Pool } = require('pg')
 // Configure the dotenv
 require('dotenv').config()
 
+const pool = new Pool({
+    user: process.env.POSTGRES_USER,
+    password: process.env.POSTGRES_PASSWORD,
+    host: process.env.HOST,
+    database: process.env.DATABASE,
+    port: process.env.PORT
+})
+
 const createConnection = async ()=>{
-    const pool = new Pool({
-        user: process.env.POSTGRES_USER,
-        password: process.env.POSTGRES_PASSWORD,
-        host: process.env.HOST,
-        database: process.env.DATABASE,
-        port: process.env.PORT
-    })
+    console.log("Tring to connect with database")
+    try{
+        const client = await pool.connect()
+        console.log("Connected")
 
-    const client = await pool.connect()
-
-    console.log("Connected")
+        return;
+    }catch(err){ throw err; }
 }
 
-module.exports = createConnection
+const query = async (text, params=null)=>{
+    const result = await pool.query(text, params)
+
+    return new Promise((resolve, reject)=>{
+        resolve(result)
+    })
+}
+
+module.exports = { createConnection, query }
 
