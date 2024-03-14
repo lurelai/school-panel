fe2a7c6f195902c1a4e5d10999ea701236bfb2e2
Lucas Ariel <github.lucas.ariel27@gmail.com>
2024-03-14 20:45:35 -0300

    :(

--- public/js/grade/main.js
+++
@@ -1,9 +0,0 @@
-import { calcGradePoint } from './calcGradePoint.js'
-import { fixUnits } from './fixUnits.js'
-import { passOrNot } from './passOrNot.js'
-import { seeChanges } from './seeChanges.js'
-
-export const main = (callBack)=>{
-    callBack({fix: fixUnits, calc: calcGradePoint, passOrNot, see: seeChanges})
-}
-
