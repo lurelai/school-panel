2f8cccee57bd4ac79d1abb6e5fb9efb4f4f37a80
Lucas Ariel <github.lucas.ariel27@gmail.com>
2024-03-22 20:03:16 -0300

    creating students

--- tests/inject-years.js
+++ tests/inject-years.js
@@ -1,16 +1,19 @@
 "use strict";
-// I am using the free API https://randomuser.me/api/?results=${ind.teacher}&inc=name
+// I am using the free API https://randomuser.me/api/?results=120&inc=name
 
 const log = new Object()
 
-const doLog = (flagMap)=>{
-	console.log(`\nStudent id entrypoint ${flagMap['-sie']}\nTeacher id entrypoint ${flagMap['-tie']}\n`)
+const doLog = ({ DF })=>{
+	console.log(`\nStudent id entrypoint ${DF['-sie']}\nTeacher id entrypoint ${DF['-tie']}\n`)
 }
 
 const flagMap = {
-	"-sie": 0, // Student id entrypoint
-	"-tie": 0  // Teacher id entrypoint
+	"-sie": "0", // Student id entrypoint
+	"-tie": "0",  // Teacher id entrypoint
+	"-y": "2024" // Year
 }
+
+let defaultConfig = {}
 
 const setFlags = ()=>{
 	const [,,...flags] = process.argv
@@ -19,19 +22,56 @@ const setFlags = ()=>{
 		const splitedFlag = flag.split('=') // [flag, falg value ]
 
 		if(!flagMap[splitedFlag[0]]){
-			console.log(`WARNING: The flag ${flag} don't exists`)
+			console.log(`WARNING: The flag ${splitedFlag[0]} don't exists`)
 			return false
 		}
 
 		flagMap[splitedFlag[0]] = splitedFlag[1]
 		return true
 	})
+
+	defaultConfig = Object.assign(defaultConfig, flagMap)
+	return true
 }
+
+
+const generate = async ()=>{
+	const { results } = await fetch('https://randomuser.me/api/?results=120&inc=name').then(e=>{ return e.json() })
+
+	const students = []
+
+	results.map(({ name: student })=>{
+		const studentObj = {
+			name:			`${student.first} ${student.last}`,
+			short_name:		`${student.last}`,
+			age:			Math.floor(Math.random() * 2) + 15,
+			id:				flagMap['-sie']++,
+		}
+
+		studentObj[defaultConfig['-y']] = {
+			"1st year": "A",
+			grade: {
+				math: [ Math.floor(Math.random() * 11), Math.floor(Math.random() * 11), Math.floor(Math.random() * 11) ],
+				english: [ Math.floor(Math.random() * 11), Math.floor(Math.random() * 11), Math.floor(Math.random() * 11) ],
+				biology: [ Math.floor(Math.random() * 11), Math.floor(Math.random() * 11), Math.floor(Math.random() * 11) ]
+			}
+		}
+
+		students.push(studentObj['2024']['grade'])
+	})
+
+	console.log(students)
+
+	return results
+}
+
 
 const init = async ()=>{
 	setFlags()
 
-	doLog(flagMap)
+	await generate()
+
+	doLog({ DF: defaultConfig })
 }
 
 init()
