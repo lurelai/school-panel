52b9c52c9fa56f8804e031ef1b0f0660f84553f3
Lucas Ariel <github.lucas.ariel27@gmail.com>
2024-04-22 20:29:32 -0300

    set the students table

--- src/database/create-tables.sql
+++ src/database/create-tables.sql
@@ -1,6 +1,7 @@
 CREATE TABLE IF NOT EXISTS students(
-	name varchar(170),
-	age varchar(10),
-	id varchar(100),
+	name varchar(170) NOT NULL,
+	nom_de_guerre varchar(100) UNIQUE NOT NULL,
+	age varchar(10) NOT NULL,
+	id varchar(100) UNIQUE NOT NULL
 );
 
