1bb74b5f0297cff8a675586663a6107129ff7b08
Lucas Ariel <github.lucas.ariel27@gmail.com>
2024-04-22 20:35:29 -0300

    set the teacher and the admin table

--- src/database/create-tables.sql
+++ src/database/create-tables.sql
@@ -1,7 +1,20 @@
 CREATE TABLE IF NOT EXISTS students(
-	name varchar(170) NOT NULL,
-	nom_de_guerre varchar(100) UNIQUE NOT NULL,
-	age varchar(10) NOT NULL,
-	id varchar(100) UNIQUE NOT NULL
+	student_name varchar(170) NOT NULL,
+	student_nom_de_guerre varchar(100) UNIQUE NOT NULL,
+	student_age varchar(10) NOT NULL,
+	student_id varchar(100) UNIQUE NOT NULL
+);
+
+CREATE TABLE IF NOT EXISTS teachers(
+	teacher_name varchar(170) NOT NULL,
+	teacher_age varchar(10) NOT NULL,
+	teacher_id varchar(100) UNIQUE NOT NULL,
+	teacher_external_id varchar(100) UNIQUE NOT NULL,
+	teacher_password varchar(100) NOT NULL
+);
+
+CREATE TABLE IF NOT EXISTS admin(
+	admin_id varchar(100) UNIQUE NOT NULL,
+	admin_password varchar(100) UNIQUE NOT NULL
 );
 
