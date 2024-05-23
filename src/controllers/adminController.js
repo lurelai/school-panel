773918787a17f9495e86fdbd13ead608c895db15
Lucas Ariel <github.lucas.ariel27@gmail.com>
2024-05-22 21:09:27 -0300

    add admin controller

---
+++ src/controllers/adminController.js
@@ -0,0 +1,17 @@
+// REGISTER -- POST --
+const register = (req, res)=>{
+	// simple for now
+	const { name, password } = req.body
+	
+	// pre-see
+	if(!adminName || !password)
+		return res.send("request dinied")
+
+	// send the info for database
+	
+
+	return res.send("new admin created")
+}
+
+module.exports = { register }
+
